f21f3cea884a6eebd06459787a106e584f423b2f
bert-csintern <jokitjo@colostate.edu>
2024-10-06 13:35:18 -0600

    added code to register socket in selectors for main program

--- server/gameServer.py
+++ server/gameServer.py
@@ -1,8 +1,22 @@
 import selectors
-
-
-
+import socket
 sel = selectors.DefaultSelector()
+
+# main server program
+host = '0.0.0.0'
+port = 60001 # 60000 + 1 for pizzaz -mallory
+
+#listening socket to register with SELECT
+
+lSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lSocket.bind((host,port))
+lSocket.listen()
+
+print("Server running on: ",host,", Listening on: ",port)
+lSocket.setblocking(False)
+sel.register(lSocket, selectors.EVENT_READ, data=None)
+
+
 # while waiting for comms from client
 try:
     while True:
