90472df3fac9b995e512cd884b58ff2dbdf1cd3b
bert-csintern <jokitjo@colostate.edu>
2024-10-25 11:37:12 -0600

    configured server to use library and message class

--- server.py
+++ server.py
@@ -13,30 +13,11 @@ def acceptWrapper(sock):
     conn, addr = sock.accept()
     print("Connection from client at: ", addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+    message = libserver.Message(sel, conn, addr)
+    sel.register(conn, selectors.EVENT_READ, data=message)
 
 
 # subroutine to service client for read or write
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
-
 
 #program starts here
 if len(sys.argv) != 3:
@@ -46,15 +27,14 @@ if len(sys.argv) != 3:
 host, port = sys.argv[1], int(sys.argv[2])
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Avoid bind() exception: OSError: [Errno 48] Address already in use
-# lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-lSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lSocket.bind((host,port))
-lSocket.listen()
+lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# lSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host,port))
+lsock.listen()
 
 print("Server running on: ",host,", Listening on: ",port)
-lSocket.setblocking(False)
-sel.register(lSocket, selectors.EVENT_READ, data=None)
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
 
 
 # while waiting for comms from client
@@ -65,7 +45,15 @@ try:
             if key.data is None:
                 acceptWrapper(key.fileobj)
             else:
-                service_connection(key, mask)
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        f"Main: Error: Exception for {message.addr}:\n"
+                    )
+                    message.close()
+
 except KeyboardInterrupt:
     print("Service interupted by admin, exiting...")
 finally:
