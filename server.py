33ae3d6205f5873714e46d9368e4102a984d3e06
bert-csintern <jokitjo@colostate.edu>
2024-12-05 11:14:50 -0700

    adding send and recieve

--- server.py
+++ server.py
@@ -111,12 +111,15 @@ def remove_player(name):
         del lobby[players[name]["SEL"]][name]
     del players[name]
 
-def create_user(conn, message):
+def create_user(conn, addr, message):
     usrn = message[1:]
     #print(usrn)
     players[usrn] = {"IP":addr[0],"GP":0,"SEL":"", "Done": False, "Name": usrn}
     log.logIt(f"User {usrn} added to current players")
-    send(conn, splash.youChose(message[1:]))
+    return usrn
+
+def send_recieve(conn, addr, sent_list):
+    pass
 
 def send(conn, message):
     message = message.encode('utf-8')
@@ -144,7 +147,8 @@ def serve_client(conn, addr):
                 break
             if message[0] == "!":
                 if setup == 0:
-                    create_user(conn, message)
+                    usrn = create_user(conn, addr, message)
+                    send(conn, splash.youChose(usrn))
                     setup += 1
                 elif setup == 1:
                     if validate(conn, message[1:]):
@@ -157,6 +161,16 @@ def serve_client(conn, addr):
             log.logIt(f"Client at addr says: {message}")
     #print(players)
     selection = players[usrn]["SEL"]
+    if serve_client_lobby(conn, addr, usrn, selection):
+        #select lobby/options operation
+    else:
+        #disconnect user operations
+        send(conn, splash.thanksForPlaying())
+        send(conn, "DISCON")
+        remove_player(usrn)
+        conn.close()
+
+def serve_client_lobby(conn, addr, usrn, selection):
     send(conn, splash.waiting())
     while True:
         if lobby_status(selection):
@@ -230,12 +244,9 @@ def serve_client(conn, addr):
             send(conn, splash.scoreBoard(players))
             if int(i) > 6:
                 break
-        
     send(conn, splash.winCondition(lobby[selection]))
-    send(conn, splash.thanksForPlaying())
-    send(conn, "DISCON")
-    remove_player(usrn)
-    conn.close()
+    #TODO Play again?
+    return True
     
 
 def run_server():
